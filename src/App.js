fc16bdd1a64724e799a253f4e918764026ee2d38
PittamHarika <pittamharika04@gmail.com>
2023-10-15 14:18:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import WordCounter from './components/countWrod';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter /> 
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*echo "# reacttasks" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PittamHarika/reacttasks.git
+git push -u origin main*/
