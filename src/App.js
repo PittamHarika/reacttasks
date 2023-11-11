9bc11f303a9706018abd93e1b18a9309b40957a0
NripeshReddy1 <nripeshreddymummadi@gmail.com>
2023-11-11 10:00:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,12 +13,3 @@ function App() {
 
 export default App;
 
-
-
-/*echo "# reacttasks" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/PittamHarika/reacttasks.git
-git push -u origin main*/
